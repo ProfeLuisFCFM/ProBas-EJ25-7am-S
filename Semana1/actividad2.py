ebcf0c270806d63bb99b1011f616cf0fa7bc48b4
HP240G <cs113invop@gmail.com>
2025-02-01 10:02:46 -0600

    Actividades Semana 1

---
+++ Semana1/actividad2.py
@@ -0,0 +1,71 @@
+
+#de = input("Quien envía: ")
+#para = input("Quien recibe: ")
+
+#mensaje = f"Feliz Navidad! inmundo animal! TQM Para: {para} De: {de}"
+
+#print(mensaje)
+
+
+numA = 40 #int(input("ingrese número a: "))
+numB = 20 #int(input("ingresa número b: "))
+
+print(numA+numB)
+
+#Tipos de dato básicos
+str()
+int()
+float()
+#bool()
+
+
+#Tipos de datos compuestos
+
+list()
+set()
+tuple()
+dict()
+
+#Tipos de datos compuestos inicializados
+for var in range(0,10,3):
+    print(var)
+
+semanaInglesa = ['Lunes','Martes','Miércoles','Jueves','Viernes']
+semanaInglesaE = [101,102,103,104,105]
+
+for itm in enumerate(semanaInglesa,101):
+    print(itm[0],itm[1])
+
+
+#Aritméticos 
+#P ()
+#E ^ **
+#M *
+#D / --> // %
+#A +
+#S -
+#Lógicos
+#Y and
+#O or
+#N Not
+#Relacionales
+#<,> desigualdades 
+#<=,>= desigualdades incluyentes 
+#!=,<> diferente
+# = igualdad
+
+
+if True:
+    print("True")
+
+if False:
+    print("True")
+else:
+    print("False")
+    
+if None:
+    print("True")
+elif False:
+    print("False")
+else:
+    print("if/elif/else")
---
+++ Semana1/ejercicio1.py
@@ -0,0 +1,12 @@
+# Descripción: Escribe un programa que solicite al usuario dos números enteros y realice las operaciones aritméticas básicas 
+# (suma, resta, multiplicación, división, división de piso y residuo) entre ellos. Muestra los resultados en pantalla.
+
+a = int(input("Ingrese el número a:"))
+b = int(input("Ingrese el número b:"))
+
+print("suma: ",a+b)
+print("resta: ",a-b, b-a)
+print("multiplica: ",a*b, b*a)
+print("divide: ",a/b, b/a)
+print("divide: ",a//b, b//a)
+print("modulo: ",a%b, b%a)
